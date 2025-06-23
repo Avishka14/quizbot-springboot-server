fd4f707cddaefe851b9d2d42301c92fc37d93c04
Avishka Chamod <avishkachamod14@gmail.com>
2025-06-23 11:17:30 +0530

    commit

---
+++ src/main/java/com/quizbot/quizbot_springboot_server/QuizbotSpringbootServerApplication.java
@@ -0,0 +1,13 @@
+package com.quizbot.quizbot_springboot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbotSpringbootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbotSpringbootServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizbot/quizbot_springboot_server/QuizbotSpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizbot.quizbot_springboot_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizbotSpringbootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
